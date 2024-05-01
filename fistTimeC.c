6f6dc707b24f7beddf078a2fc0bfd1b947586192
zuccc23 <daniyaahmane@gmail.com>
2024-05-02 00:23:10 +0200

    commit first c file

---
+++ fistTimeC.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{ 
+	double peach = 7.9 ;
+	
+	printf ("hello world\nyou ugly %f \n" , peach ) ;
+	return 0 ; // commentaire :p  
+}
+/* bla bla bla bla bla
+encore bla bla bla */ 
+
