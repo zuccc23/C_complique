993d4a07a8c49c1da02ab144301b3ddd64dc3b5e
zuccc23 <daniyaahmane@gmail.com>
2024-05-06 16:47:49 +0200

    edited cthing

--- cthing.c
+++ cthing.c
@@ -1,10 +1,11 @@
+// print some text with a decimal number
 #include <stdio.h>
 int main ()
 { 
-	double peach = 7.9 ;
+	double peach = 7.9 ; 
 	
-	printf ("hello world\nyou ugly %f \n" , peach ) ;
-	return 0 ; // commentaire :p  
+	printf ("hello world\neat your vegetables you will be %f percent more handsome \n" , peach ) ;
+	return 0 ; // comment :p  
 }
 /* bla bla bla bla bla
 encore bla bla bla */ 
