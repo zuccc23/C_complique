64ee64322c639381475197f41b07c039d4b14870
zuccc23 <daniyaahmane@gmail.com>
2024-05-06 15:32:18 +0200

    learnc is here

---
+++ learnc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{ 
+
+	int minou = 0 ;
+	while (minou < 12)
+	{ 
+		if (minou == 1)
+		{
+			printf ("this year your cat is %d year old\n" , minou) ;
+		}
+	        else 
+		{
+			printf ("this year your cat is %d years old \n" , minou) ;
+		}
+	       minou = minou + 1 ;	
+	}
+}
+
+
