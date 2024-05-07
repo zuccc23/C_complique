28824d4a3e977f7354e952386d2d5496c7c86a70
zuccc23 <daniyaahmane@gmail.com>
2024-05-07 13:37:33 +0200

    jsp

--- learnc.c
+++ learnc.c
@@ -1,18 +1,17 @@
+// 
 #include <stdio.h>
 #include <stdlib.h>
 int main ()
 { 
 
-	printf ("Choose 2 numbers to multiply \n") ;
-	int saiki = 0 , nendo = 0 , kaido = 0 ;
-	scanf( "%d", &saiki) ;
-	scanf("%d" , &nendo) ;
+int saiki ;
+for (saiki = 0 ; saiki <= 10 ; saiki++)
+{
+	printf("yare yare \n");
 
-	kaido = saiki * nendo ;
+}
 
-	printf ("%d * %d = %d \n" , saiki , nendo , kaido ) ;
 
-	
 	
 }
 
