1799c35af6e0b852b3789d8c521a7e79f6a368c7
zuccc23 <daniyaahmane@gmail.com>
2024-05-07 15:20:07 +0200

    exo 4

---
+++ exc4.c
@@ -0,0 +1,18 @@
+//declare and use functions 
+#include <stdio.h>
+
+
+
+int flop(int number)
+{
+	return number * 2 ;
+}
+
+
+int main() 
+{
+int result = flop(4) ;
+printf("you have %d bitches", result);
+
+}
+
