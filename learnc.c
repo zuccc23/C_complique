b7fad8c0fc8eb167292f32131c379ca2e3e56c1d
zuccc23 <daniyaahmane@gmail.com>
2024-05-10 18:00:07 +0200

    update learnc

--- learnc.c
+++ learnc.c
@@ -1,22 +1,41 @@
-//declare and use functions 
+// 
 #include <stdio.h>
-#include "learnc.h"
+#include <time.h>
+#include<stdlib.h>
 
-void pom()
+int main ()
 {
-	hello() ;
+
+int given = 0;
+srand(time(NULL)) ;
+int number1 = (rand()%100)+1;
+
+while (!(given==number1))
+{
+	printf("can you guess the number?\n");
+	scanf("%d",&given);
+	if (given>number1)
+	{
+		printf("too big\n");
+	}
+
+	else if (given<number1)
+	{
+		printf("too small\n");
+	}
+	else 
+	{
+		printf("correct!\n");
+	}
 }
 
-void hello()
-{
-	printf("hello\n");
-} 
 
-int main() 
-{
-pom() ;
+return 0;
 
 }
+
+
+
 
 
 
