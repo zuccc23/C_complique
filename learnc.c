558ab6a9a10193d71cadd954bf3d227405601702
zuccc23 <daniyaahmane@gmail.com>
2024-05-16 23:57:08 +0200

    learnc update

--- learnc.c
+++ learnc.c
@@ -1,30 +1,21 @@
-// 
+//utiliser un tableau 
 #include <stdio.h>
-#include <time.h>
 #include<stdlib.h>
 
-void calcul(int *pointeur1);
+
 
 int main ()
 {
 
-	int nombre = 6;
-	calcul( &nombre);
-	printf("le double de nombre est : %d\n",nombre );
-	return 0;
+int poyo[5];
+poyo[0]=44 ;
+poyo[1]=66 ;
+poyo[2]=88 ;
+poyo[3]=99 ;
+poyo[4]=11 ;
 
-
+printf ("%p\n", &poyo);
 }
-
-
-
-void calcul(int *pointeur1)
-{
-	*pointeur1 *= 2;
-
-}
-
-
 
 
 
