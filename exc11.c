53863f564c11a4441cc1d634c8ac253b812fa19c
zuccc23 <daniyaahmane@gmail.com>
2024-05-16 23:56:17 +0200

    exc11

---
+++ exc11.c
@@ -0,0 +1,20 @@
+//utiliser un tableau 
+#include <stdio.h>
+#include<stdlib.h>
+
+
+
+int main ()
+{
+
+int poyo[5];
+poyo[0]=44 ;
+poyo[1]=66 ;
+poyo[2]=88 ;
+poyo[3]=99 ;
+poyo[4]=11 ;
+
+printf ("%d\n", poyo[2]);
+}
+
+
