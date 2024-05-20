d47cc221b3d937e775918737619dedda1cbb9edb
zuccc23 <daniyaahmane@gmail.com>
2024-05-20 23:50:18 +0200

    learnc update

--- learnc.c
+++ learnc.c
@@ -2,39 +2,8 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-int plus(int tabl[], int num);
-double moy(int *pom ,int num) ;
-
 int main()
 {
-	int notes[7]={2,7,12,14,13,13,18};
-	plus (notes,7);
-	int pompom = plus (notes, 7);
-	moy(&pompom, 7);
-	double popy = moy(&pompom, 7);
-	printf("addition of all results is %d\naverage of all results is %f\n", pompom, popy);
-
-	
+char lettre = 'b';
+printf("%c", lettre);
 }
-
-int plus(int tabl[], int num)
-{
-	int i = 0;
-	int result = 0;
-
-	for (i=0; i<num; i++)
-	{
-		result += tabl[i];
-	}
-
-return result;
-
-}
-
-double moy(int *pom ,int num)
-{
-	double fres=(double) *pom/ (double) num;
-	return fres;
-}
-
-
