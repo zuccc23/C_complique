1ae2670da7ad8d838c5ce14da73ef60f0f3a9e45
zuccc23 <daniyaahmane@gmail.com>
2024-05-26 16:09:35 +0200

    get users name

---
+++ exc17.c
@@ -0,0 +1,25 @@
+//get the user's name!
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+char yap[20]={0};
+
+int i=0;
+printf("whats your name?\n");
+for (i=0;i<20;i++)
+{
+	scanf("%c", &yap[i]);
+	if (yap[i]=='\n')
+	{
+		break;
+	}
+
+}
+
+printf("your name is %s\n", yap);
+
+}
+
+
