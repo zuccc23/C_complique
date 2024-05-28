ab75d7bdafd30890993b4dd404066f3861331c39
zuccc23 <daniyaahmane@gmail.com>
2024-05-28 19:48:53 +0200

    users name but better

---
+++ exc18.c
@@ -0,0 +1,15 @@
+//print users name with string
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+char yap[]={'0'};
+
+printf("whats your name?\n");
+scanf("%s", &yap[0]);
+
+printf("your name is %s? ugly ass name\n", yap);
+
+}
+
