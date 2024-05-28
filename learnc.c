884c1a2b946ca4672d906edbffa76b3ae0c5166b
zuccc23 <daniyaahmane@gmail.com>
2024-05-28 19:49:20 +0200

    update learnc

---
+++ learnc.c
@@ -0,0 +1,16 @@
+//chaîne de charactère
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+char yap[]={'0'};
+
+printf("whats your name?\n");
+scanf("%s", &yap[0]);
+
+printf("your name is %s? ugly ass name\n", yap);
+
+}
+
+
