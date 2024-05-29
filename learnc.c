2762b68477f1ea897df3cdc7501afe054e40c7f0
zuccc23 <daniyaahmane@gmail.com>
2024-05-29 21:15:13 +0200

    learnc update

--- learnc.c
+++ learnc.c
@@ -1,16 +1,34 @@
 //chaîne de charactère
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include "learnc.h"
+void calc_cat(int catage[]);
 
 int main()
 {
-char yap[]={'0'};
+int cat_age[20]={0};
 
-printf("whats your name?\n");
-scanf("%s", &yap[0]);
-
-printf("your name is %s? ugly ass name\n", yap);
+calc_cat(cat_age);
+print_tableint(cat_age,20);
 
 }
 
 
+
+void calc_cat(int catage[])
+{
+	catage[0]=0;
+	catage[1]=18;
+	catage[2]=24;
+
+	int i;
+	for (i=3; i<20; i++)
+	{
+		int p = i-1;
+		catage[i]=catage[p]+4;
+	}
+
+
+}
