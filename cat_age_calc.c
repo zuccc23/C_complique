0fb39cb1dbd872b86203abf9b484f521c8a79e06
zuccc23 <daniyaahmane@gmail.com>
2024-05-31 23:59:18 +0200

    update

--- cat_age_calc.c
+++ cat_age_calc.c
@@ -4,23 +4,23 @@
 #include<string.h>
 #include<time.h>
 #include "learnc.h"
-void calc_cat(int catage[]);
+void calc_cat(int catage[]); //prototype
 
 int main()
 {
-int cat_age[39]={0};
+int cat_age[39]={0}; //set table to 39 slots cuz cats cant be that old
 
-calc_cat(cat_age);
-//print_tableint(cat_age,39);
+calc_cat(cat_age);//call the function
+
 
 int age =0;
 printf("how old is your kitty?\n");
-scanf("%d", &age);
+scanf("%d", &age); //get the cats age from user
 
 if (age<39)
 {
 	printf("your cat is %d years old in human years\n", cat_age[age]);
-}
+} //print table depending on age given by user
 
 else
 {
@@ -35,14 +35,14 @@ void calc_cat(int catage[])
 {
 	catage[0]=0;
 	catage[1]=18;
-	catage[2]=24;
+	catage[2]=24; //setting early cat ages
 
 	int i;
 	for (i=3; i<39; i++)
 	{
 		int p = i-1;
 		catage[i]=catage[p]+4;
-	}
+	} /*function that adds 4 each year cuz thats how cat age to human age is calculated apparently*/
 
 
 }
