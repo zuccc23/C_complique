d900de31e0b8483cfb59dbd92c4f5559d2061d76
zuccc23 <daniyaahmane@gmail.com>
2024-06-01 23:36:47 +0200

    functions of string.h 3

--- exc21.c
+++ exc21.c
@@ -24,6 +24,40 @@ char uchiyamada1[]="ONIZUKAAAAAAAAAA";
 strcat(fuyutsuki, uchiyamada1);
 printf("%s\n", fuyutsuki);
 
+//compare 2 strings
+char kunio[]="je deteste onizuka";
+char tomoko[]="j'aime onizuka";
+char urumi[]="je deteste onizuka";
+
+if (strcmp(kunio, tomoko)==0)
+{
+	printf("kunio et tomoko sont d'accord\n");
+}
+else
+{
+	printf("kunio et tomoko sont pas d'accord\n");
+}
+
+if (strcmp(kunio, urumi)==0)
+{
+	printf("kunio et urumi sont d'accord\n");
+}
+else
+{
+	printf("kunio et urumi sont pas d'accord\n");
+}
+
+//search a character w strchr
+
+int main()
+{
+char onini[]="je m'appelle eikichi onizuka, 22ans célibataire et libre comme l'air";
+char *findephrase; //pointeur ds lequel va être stocké le résultat de strchr
+int leO='o';//caractère recherché
+findephrase=strchr(onini, leO);//fonction qui prend la chaine et le caractère 
+printf("%s\n", findephrase);//imprime le pointeur qui contient le reste du string à partir du caractère recherché
+}
+
 
 
 }
