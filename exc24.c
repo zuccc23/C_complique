de92f632efbec82a1177ad25c2846ce684ef636a
zuccc23 <daniyaahmane@gmail.com>
2024-09-03 17:06:23 +0200

    use structures in functions

---
+++ exc24.c
@@ -0,0 +1,20 @@
+//popopopopop
+#include <stdlib.h>
+#include <stdio.h>
+#include "learnc.h"
+
+int main()
+{
+kitty star; //creating a variable 'star' of previously created type 'kitty'
+zero(&star);//calling the function for the variable star 
+printf("kuromi is currently %d\n", star.kuromi);
+star.kuromi=9; //using the 'kuromi' variable in the type 'kitty' as the variable 'star'
+printf("kuromi is now %d, and pompom is %d\n", star.kuromi, star.pompom);
+}
+
+int zero(kitty *star) //function that assigns variables of a structure to 0
+{
+	(*star).kuromi=0;//accessing the star.kuromi variable in a function
+	star->pompom=0;//same but easie to write
+return 0;
+}
