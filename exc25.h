2bb47ff8210bec008eefb938ae8d527f854a63fb
zuccc23 <daniyaahmane@gmail.com>
2024-09-11 17:35:31 +0200

    .h file

---
+++ exc25.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef enum Volume Volume; //create a type of enumeration type 'Volume'
+enum Volume
+{
+	FAIBLE =10, MOYEN=50, FORT=100 //set the enum values to int values
+};
+
+
