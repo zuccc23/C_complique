9c5e58a23219747fa0ea13babc1e740e779ec449
zuccc23 <daniyaahmane@gmail.com>
2024-09-11 17:35:11 +0200

    use enum

---
+++ exc25.c
@@ -0,0 +1,13 @@
+//popopopopop
+#include <stdlib.h>
+#include <stdio.h>
+#include "learnc.h"
+
+
+int main()
+{
+Volume son = FORT; //create a variable 'son' of type 'Volume' and set it to FORT
+printf("le son est à %u%%\n", son); //print result
+
+}
+
